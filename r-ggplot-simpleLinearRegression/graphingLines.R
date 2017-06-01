46625ecb0142ee8e9aa3f6bfa9d1b6ba1e362330
david-adr-elizalde <davir.adr.elizalde@hpe.com>
2017-06-01 13:58:46 -0500

    ggplot2 line update

--- r-ggplot-simpleLinearRegression/graphingLines.R
+++ r-ggplot-simpleLinearRegression/graphingLines.R
@@ -1,8 +1,15 @@
 #Graphing lines
+library(ggplot2)
 
 x<-c(1,8)
 y<-c(3,10)
 dat<-data.frame(x,y)
 
-library(ggplot2)
+
 ggplot()+geom_line(data=dat,aes(x=x,y=y),size=1,color="blue")
+
+a<-c(0,10)
+b<- -5*a+1
+datos<-data.frame(a,b)
+ggplot()+geom_line(data=dat,aes(x=a,y=b),size=1,color="blue")
+
