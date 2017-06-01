39a7006f2c883acc8d7e0db2be557e4f5561a1df
david-adr-elizalde <davir.adr.elizalde@hpe.com>
2017-06-01 13:46:33 -0500

    update ggplot2 graph

--- r-ggplot-simpleLinearRegression/ggplot2-first-graph.R
+++ r-ggplot-simpleLinearRegression/ggplot2-first-graph.R
@@ -11,9 +11,10 @@ ggplot()+geom_point(data=dat,aes(x=a,y=b),size=10,color="red")
 x<-c(2,5,1)
 y<-c(6,4,9)
 datos<-data.frame(x,y)
-ggplot()+geom_point(data=datos,aes(x=x,y=y),size=6,color="green")+
-  scale_x_continuous(limits=c(0,15),breaks=seq(0,15,1))+
-  scale_y_continuous(limits=c(0,15),breaks=seq(0,15,1))
+ggplot()+geom_point(data=datos,aes(x=x,y=y),size=6,color="darkorange",shape=10)+
+  scale_x_continuous(limits=c(0,15),breaks=seq(0,15,5))+
+  scale_y_continuous(limits=c(0,15),breaks=seq(0,15,5))
 
 seq(0,15,1)
 seq(0,15,5)
+
