c55cd9ce4f3bc55e56036fc2f1b9831380786a19
david-adr-elizalde <davir.adr.elizalde@hpe.com>
2017-06-02 08:10:23 -0500

    adding vertical samples

---
+++ r-ggplot-simpleLinearRegression/samplealongline.R
@@ -0,0 +1,19 @@
+#Samples along a line
+#Create four vertical samples of 100 points each
+#Means must lie on the line y = 3x + 1
+#x-locations: 1,9,15,22
+
+x<-c(0,25)
+y<-3*x + 1
+line<-data.frame(x,y)
+
+library(ggplot2)
+
+x<-c(1,9,15,22)
+y<-3*x + 1
+means<-data.frame(x,y)
+
+ggplot()+
+  geom_line(data=line,aes(x=x,y=y))+
+  geom_point(data=means,aes(x=x,y=y),size=7,color="red")
+
