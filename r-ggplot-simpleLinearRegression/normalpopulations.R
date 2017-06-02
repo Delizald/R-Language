ccd55504e6fe3602b9dcd02dea5f26c85422a8bb
david-adr-elizalde <davir.adr.elizalde@hpe.com>
2017-06-01 19:41:30 -0500

    adding normal pop sample

---
+++ r-ggplot-simpleLinearRegression/normalpopulations.R
@@ -0,0 +1,15 @@
+#Normal populations
+sample(1:10,100,replace=TRUE) #number 1 to 10 x 100 times
+
+
+#Mean
+#*Numbers sampled will be relatively close to the mean
+#*There is an equal chance of a number being above the mean as there is of it being below the mean
+
+#Standard Deviation
+#*Tells you how spread out you can epect your sample to be
+#*If the population has a large standard deviation, essentially you 
+#will be selecting numbers over a wider range.
+
+rnorm(100,50,10)
+
