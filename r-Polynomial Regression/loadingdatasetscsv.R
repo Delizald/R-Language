d4b11b39767d9f8450490992be37b57002e98b70
david-adr-elizalde <davir.adr.elizalde@hpe.com>
2017-06-07 08:47:46 -0500

    updating example

--- r-Polynomial Regression/loadingdatasetscsv.R
+++ r-Polynomial Regression/loadingdatasetscsv.R
@@ -16,3 +16,18 @@ function(x){
 ggplot()+
   geom_point(data=dat,aes(x=x,y=y))+
   stat_function(data=data.frame(x=c(-5,15)),aes(x=x),fun=f)
+  geom_point(data=means,aes(x=x,y=y),color="red",size=3)
+
+dat$group<-1:100
+means$group<-1:100
+
+groups<-rbind(dat,means)
+
+ggplot()+
+  geom_point(data=dat,aes(x=x,y=y))+
+  stat_function(data=data.frame(x=c(-5,15)),aes(x=x),fun=f)
+  geom_point(data=means,aes(x=x,y=y),color="red",size=3)+
+  geom_line(data=groups,aes(x=x,y=y,group=group))
+  
+sum((dat$y-means$y)^2)
+
